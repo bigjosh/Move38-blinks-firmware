5a4ddbfd452fb28214c8990f19db59d5562509e3
Josh Levine <github@junk.josh.com>
2017-01-16 11:28:19 -0500

    Create an AS7 project to make things easier to work on

---
+++ TileMain.c
@@ -0,0 +1,18 @@
+/*
+ * Move38-blinks-firmware.c
+ *
+ * Test code to compile and put the Tile Core Firmware though its paces. 
+ *
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
